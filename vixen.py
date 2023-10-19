66f6831424c89cd3b6b2d9d46226f4f84bc9b5a8
Mal <mariuscastillo0512@gmail.com>
2023-10-18 22:29:49 -0400

    better

--- vixen.py
+++ vixen.py
@@ -1,19 +1,22 @@
 x = 0
 y = 0
-
+#defining function
 def quadruple(x):
     if x == 4:
         y = x ** 2
     else:
         y = x * 4
     return y
-x = int(input("yo whatchu want x to be? "))
+#user input
+x = quadruple(int(input("yo whatchu want x to be? ")))
 print("okay bud imma do you a favor an quadruple that")
+#generic statements
 if y>16:
     print("bruh its over 16")
 elif y==16:
     print("jackpot.")
 else:
     print("my guy, its under 16")
-print("at a total value of", y, sep=" ", end="\n")
+
+print("at a total value of", x, sep=" ", end="\n")
 print("have a good day!")
