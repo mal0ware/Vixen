7b97f10c2d85bab43fa554011b274a6498bfa300
Mal <mariuscastillo0512@gmail.com>
2024-07-24 18:10:50 -0400

    ?

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('messageCreate', async (message) => {
     const command = message.content.toLowerCase();
     if (commands[command]) {
       message.reply(commands[command]);
-      console.log(commands[command]);
+      console.log("lol");
     }
   });
  
