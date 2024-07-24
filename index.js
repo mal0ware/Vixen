5a854df5f75df387ad89419b921cd1b9280fbb39
Mal <mariuscastillo0512@gmail.com>
2024-07-24 19:49:28 -0400

    commenting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
- 
+ //V.I.X.E.N.
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "MESSAGE_CONTENT"],
     partials: ["CHANNEL", "MESSAGE"]
 });
  
-const token = ("REDACTED-DISCORD-TOKEN") // your bot token here
+const token = ("REDACTED-DISCORD-TOKEN")
  
 client.on('ready', async () => {
     console.log(`Client has been initiated! ${client.user.username}`)
