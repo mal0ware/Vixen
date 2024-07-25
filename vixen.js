8a06664dfd7df49340e924e49c8b697a15303c4e
Mal <mariuscastillo0512@gmail.com>
2024-07-24 23:22:37 -0400

    test

--- vixen.js
+++ vixen.js
@@ -6,16 +6,34 @@ const client = new Discord.Client({
 });
  
 const token = ("OTMzNDk1MTI5NTM1MzA3ODE2.GLctK6.OPOS1MG4gtnVCIoZN1HV-l7sbiNdyW9PN4SDOU")
- 
+client.commands = new Discord.Collection();
+
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 client.on('ready', async () => {
-    console.log(`Client has been initiated! ${client.user.username}`)
+  console.log(`Client has been initiated! ${client.user.username}`);
 });
- 
-client.on('messageCreate', async (message) => {
-    if (message.content.toLowerCase() === "test") {
-        console.log("slur has been said.")
-        message.reply("nigga stfu");
-    }
+
+client.on('messageCreate', async message => {
+  if (!message.content.startsWith(prefix)) return; // You can set a prefix here
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+  if (!command) return;
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error executing that command!');
+  }
 });
- 
+
 client.login(token);
